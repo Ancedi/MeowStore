3aca7e2ba578a9cb9119aa2b41d02687a8770b6e
Ancedi <patrickhombrebueno@gmail.com>
2026-05-05 10:31:58 +0200

    Updated Catalogue Css File, Changed File Path, API Call fully functional.

--- script/loadGallery.js
+++ script/loadGallery.js
@@ -11,8 +11,7 @@ async function LoadGallery(){
             const clone = template.content.cloneNode(true);
             clone.querySelector("h3").textContent = cat.breeds[0].name;
             clone.querySelector("p").textContent = cat.breeds[0].description;
-            clone.querySelector("img").src = cat.url ?
-             `https://api.thecatapi.com/v1/images/${cat.url}.jpg` : "pics/cute-smiling-cat-flat-style-doodle-cartoon.png";
+            clone.querySelector("img").src = cat.url ?? "pics/cute-smiling-cat-flat-style-doodle-cartoon.png";
 
             container.appendChild(clone);
             // Code to display each cat in the gallery
